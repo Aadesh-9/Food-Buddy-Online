5b7017f7d653acc9debdba2873e2db77b6d2cdd8
Aadesh Wasamkar <aadeshwasamkar@gmail.com>
2025-01-12 14:27:24 +0530

    core react - old way of writing react without using jsx

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ import React from "react";
 import ReactDOM from "React-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("h1", { id: "heading1" }, "Aadesh"),
-  React.createElement("h1", { id: "heading2" }, "Wasamkar"),
+  React.createElement("div", { id: "parent1" }, [
+    React.createElement("h1", { id: "heading1" }, "Aadesh"),
+    React.createElement("h1", { id: "heading2" }, "Wasamkar"),
+  ]),
+  React.createElement("div", { id: "parent2" }, [
+    React.createElement("h1", { id: "heading3" }, "Aadesh"),
+    React.createElement("h1", { id: "heading4" }, "Wasamkar"),
+  ]),
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
